82ae970b42e8ff2a05a449f5545e3031f46d34a5
lukasdamion91 <luke.damion@getblownhair.biz>
2026-08-23 17:09:38 +1000

    V25.3: enable Clerk sign-in-or-up transfer

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -48,6 +48,7 @@ export default function Login() {
         {authConfigured ? (
           <SignIn
             routing="hash"
+            withSignUp={true}
             signUpUrl="/join"
             fallbackRedirectUrl={redirect}
             forceRedirectUrl={redirect}
